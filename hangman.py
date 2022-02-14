37a7ebfbabc2ad7cfb4dfb69fa54483acf20e22d
“tsavycheva” <tsavycheva@student.ucode.world>
2022-02-14 17:24:05 +0200

    Helmed it

--- hangman.py
+++ hangman.py
@@ -1,4 +1,14 @@
+from curses.ascii import isalnum, isalpha
+from enum import unique
+from gettext import find
+from operator import index
+from pickle import FALSE, TRUE
+from posixpath import isabs
 import random
+from tkinter import E, W
+from tracemalloc import start
+from turtle import right
+from unicodedata import numeric
 
 hangman = (
 """
@@ -97,34 +107,60 @@ def get_word(file):
 
 def play(word):
     #word = get_word('words.txt') #слово, которое нужно будет угадывать
-    print(word)
-    one_letter = "[?]" * len(word) #по oдному дефису на каждую букву, которую нужно отгадать
+    #print(word)
+    #one_letter = "[?]" * len(word) #по oдному дефису на каждую букву, которую нужно отгадать
     wrong = 0 #количество ошибок
     used = [] #буквы, которые уже отгадали
+    right_suggested = []
 
-    while wrong < max_wrong and one_letter != word: #не равно
+    while wrong < max_wrong and sorted(right_suggested) != sorted(''.join(set(word))): #and one_letter != word: #не равно
         print(GREEN + hangman[wrong] + ENDC)
         print("\n`Used letters`\n", used)
-        print("\nYour word:\n", one_letter) 
+        
+        #hello-opw
+        #hop
+
+        for pos,char in enumerate(word):
+            if(char in used):
+                print(" " + word[pos], end=" ")
+            elif (word[pos] == "-"):
+                print(" - ", end="")
+            else:
+                print(" _ ", end="")
+
+        #Печать слова
+
         #то что я ввожу в консоле
         guess = input("\n\nGuess a letter: ") #ввод
-        while guess in used:
-            print("You already used this letter: ", guess)
-            guess = input("\n\nGuess the letter: ")
-        used.append(guess) #добавляю один элемент в конец списка
+
+        #while guess in used:
+            #print("You already used this letter: ", guess)
+            #guess = input("\n\nGuess the letter: ")
         #проверяю наличие буквы в слове
-        new = ""
-        if guess in word:
+        #new = ""
+        if len(guess) != 1 or not isalpha(guess):
+            print("Unexpected char")
+
+        elif guess in word and guess not in used:
             print("\You are right! The letter", guess, "is in the word")
-            for letter in range(len(word)): #если буква в списке 
-                if guess == word[letter]:
-                    new += guess #прибавит к списку
-                else:
-                    new += one_letter[letter]
-            one_letter = new
+            used.append(guess)
+            right_suggested.append(guess)
+            
+        elif guess in used:
+            print("\You already used this letter")
+
+            #for letter in range(len(word)): #если буква в списке
+                #if guess == word[letter]:
+                    #new += guess #прибавит к списку
+                #else:
+                    #new += one_letter[letter]
+            #one_letter = new
         else:
             print("\nThe letter", guess, "is`n in word.")
             wrong += 1
+            used.append(guess)
+
+        #добавляю один элемент в конец списка
     
     #завершение игры
     if wrong == max_wrong:
